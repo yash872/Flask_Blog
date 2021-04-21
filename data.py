af597fa3cf5764419128dd4cb182bcfbdf83b2b5
yash872 <ridic143@gmail.com>
2021-04-21 18:32:10 +0530

    Added the whole stucture

---
+++ data.py
@@ -0,0 +1,28 @@
+def Articles():
+    articles = [
+
+        {
+            'id':1,
+            'title':'Article One',
+            'body': 'This is the Article one',
+            'author': 'Yash Bhawsar',
+            'created_date': '08-04-1996'
+        },
+        {
+            'id':2,
+            'title':'Article two',
+            'body': 'This is the Article one',
+            'author': 'Mahesh Bhawsar',
+            'created_date': '29-10-1971'
+        },
+        {
+            'id':3,
+            'title':'Article Three',
+            'body': 'This is the Article one',
+            'author': 'Jyoti Bhawsar',
+            'created_date': '09-07-1975'
+        }
+    
+    ]
+
+    return articles
